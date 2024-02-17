86ba3632305db9564aa53a5f6c3d21395c36095f
Anderson Braz <contato@andersonbraz.com>
2024-02-17 17:27:32 -0300

    Up Files(s).

--- sample.py
+++ sample.py
@@ -5,39 +5,29 @@ from time import sleep
 # Acessa a URL
 driver = webdriver.Edge()
 driver.get("https://dados.cvm.gov.br/organization/cvm")
-
 # Espera o carregamento da página
 sleep(5)
-
 # Localiza o campo de pesquisa
-campo_pesquisa = driver.find_element(By.ID, "field-giant-search")
-
+campo_pesquisa = driver.find_element("id", "field-giant-search")
 # Preenche o campo de pesquisa
 campo_pesquisa.send_keys("Fundos de Investimento + Documentos + Extrato das Informações")
-
 # Localiza o botão de pesquisa
-botao_pesquisa = driver.find_element(By.XPATH, "//button[@aria-label='Submit' and @value='search']")
-
+botao_pesquisa = driver\
+    .find_element("xpath", "//button[@aria-label='Submit' and @value='search']")
 # Clica no botão de pesquisa
 botao_pesquisa.click()
-
 # Espera o resultado da pesquisa
 sleep(5)
-
-link_dataset = driver.find_element(By.CSS_SELECTOR, "a[href='/dataset/fi-doc-extrato'][data-format='csv']")
-
+link_dataset = driver\
+    .find_element("css selector", "a[href='/dataset/fi-doc-extrato'][data-format='csv']")
 # Clica no link do dataset
 link_dataset.click()
-
 sleep(5)
-
 # Localiza o link para o extrato de fundos de investimento
-link_extrato = driver.find_element(By.CSS_SELECTOR, "a.heading[title='Extratos de Fundos de Investimento (2024)']")
-
+link_extrato = driver.\
+    find_element(By.CSS_SELECTOR, "a.heading[title='Extratos de Fundos de Investimento (2024)']")
 # Clica no link do extrato
 link_extrato.click()
-
 sleep(15)
-
 # Fecha o navegador
 driver.close()
--- workflow-cvm.py
+++ workflow-cvm.py
@@ -1,8 +1,7 @@
 from solutions.cvm.jobs.step_01_extract import JobExtract
 import utils.handlers.handler_json as handler
 
+lista_navegacao = handler.get_json_to_list("data/cvm/navigation_cvm_fi.json")
 
-lista_arquivos = handler.get_json_to_list("data/cvm/files.json")
-
-job_extract = JobExtract(lista_arquivos)
+job_extract = JobExtract(lista_navegacao)
 df_step_01 = job_extract.run_tasks()
