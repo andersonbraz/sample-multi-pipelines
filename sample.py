822bd06265fd0cd12694ace5ae31d3638ba44bf7
Anderson Braz <andersonbraz@mac-braz.com>
2024-03-21 00:00:04 -0300

    Update repository.

--- sample.py
+++ sample.py
@@ -1,33 +1,39 @@
 from selenium import webdriver
-from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
 from time import sleep
 
 # Acessa a URL
 driver = webdriver.Edge()
 driver.get("https://dados.cvm.gov.br/organization/cvm")
-# Espera o carregamento da página
-sleep(5)
-# Localiza o campo de pesquisa
+wait = WebDriverWait(driver, 15)
+
+# Step 1 - Pesquisa
 campo_pesquisa = driver.find_element("id", "field-giant-search")
-# Preenche o campo de pesquisa
 campo_pesquisa.send_keys("Fundos de Investimento + Documentos + Extrato das Informações")
-# Localiza o botão de pesquisa
-botao_pesquisa = driver\
-    .find_element("xpath", "//button[@aria-label='Submit' and @value='search']")
-# Clica no botão de pesquisa
+
+# Step 2 - Navegacao
+botao_pesquisa = driver.find_element("xpath", "//button[@aria-label='Submit' and @value='search']")
 botao_pesquisa.click()
-# Espera o resultado da pesquisa
 sleep(5)
+
+# Step 3 - Navegacao
 link_dataset = driver\
     .find_element("css selector", "a[href='/dataset/fi-doc-extrato'][data-format='csv']")
-# Clica no link do dataset
 link_dataset.click()
 sleep(5)
-# Localiza o link para o extrato de fundos de investimento
+
+
+# Step 4 - Navegacao
 link_extrato = driver.\
-    find_element(By.CSS_SELECTOR, "a.heading[title='Extratos de Fundos de Investimento (2024)']")
-# Clica no link do extrato
+    find_element("css selector", "a.heading[title='Extratos de Fundos de Investimento (2024)']")
 link_extrato.click()
-sleep(15)
+sleep(5)
+
+# Step 5 - Download
+link_download = driver.\
+    find_element("css selector", "a.btn.btn-primary.resource-url-analytics")
+link_download.click()
+sleep(5)
+
 # Fecha o navegador
 driver.close()
